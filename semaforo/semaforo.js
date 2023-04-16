6fdfb0b4a03e929ffd9da40acf276db3608c183f
ingryd <ingryddasilva7@gmail.com>
2023-04-16 11:53:27 -0300

    projeto semaforo

--- semaforo/semaforo.js
+++ semaforo/semaforo.js
@@ -5,26 +5,45 @@ const img = document.getElementById('img')
 const btns = document.getElementById('btns')
 
 let colorIndex = 0;
+let intervalID = null;
 
 const trafficLight = (event) => {
     //primeiro jeito de chamar um metodo:
     //turnOn.vermelho()
 
     //segundo jeito:
+    stopAutomatic ();
     turnOn[event.target.id]();
+    stopAutomatic
+}
+
+const nextIndex = () => {
+    if (colorIndex < 2){
+        colorIndex ++
+    } else {
+        colorIndex = 0
+    }
+
+    //posso fazer a validação assim tb:
+    //const nextIndex = () => colorIndex = colorIndex < 2? ++colorIndex : 0;
 }
 
 const changecolor = () => {
     const colors = ['vermelho', 'amarelo', 'verde']
     const color = colors [colorIndex];
-    turnOn[color]()
+    turnOn[color]();
+    nextIndex()
+}
+
+const stopAutomatic = () => {
+    clearInterval (intervalID)
 }
 
 const turnOn = {
     'vermelho': () => img.src = './img/vermelho.png',
     'amarelo': () => img.src = './img/amarelo.png',
     'verde': () => img.src = './img/verde.png',
-    'automatico': () => setInterval (changecolor, 1000)
+    'automatico': () => intervalID = setInterval (changecolor, 500)
 };
 
 btns.addEventListener('click', trafficLight);
